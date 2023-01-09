ebca9824c5332f1c19a30d028f92bae8abeb2481
ManPanchani <manpanchani0511@gmail.com>
2023-01-09 19:27:00 +0530

    Gallery App

---
+++ lib/screen/gollbal.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Gollbal {
+  static List Images = [
+    {
+      "Name": "Taj Mahal",
+      "Image": "images/taj mahal 1.jpeg",
+      "Image1": "images/taj mahal 2.jpeg",
+    },
+    {
+      "Name": "Kerala",
+      "Image": "images/kerala 1.jpeg",
+    },
+    {
+      "Name": "Kutch",
+      "Image": "images/kutch 1.jpeg",
+    },
+    {
+      "Name": "Qutub Minar",
+      "Image": "images/qutub minar 1.jpg",
+    },
+    {
+      "Name": "Somnath Mandir",
+      "Image": "images/somnath 1.jpeg",
+    },
+    {
+      "Name": "Gir",
+      "Image": "images/gir 1.jpeg",
+    },
+  ];
+
+  // List ImagesSlider = [
+  //   [
+  //     {"Name": "Taj Mahal", "Image": "images/taj mahal 1.jpeg"},
+  //     {"Name": "Taj Mahal", "Image": "images/taj mahal 2.jpg"},
+  //     {"Name": "Taj Mahal", "Image": "images/taj mahal 3.jpeg"},
+  //     {"Name": "Taj Mahal", "Image": "images/taj mahal 4.jpg"},
+  //   ],
+  //   [
+  //     {"Name": "Kerala", "Image": "images/kerala 1.jpeg"},
+  //     {"Name": "Kerala", "Image": "images/kerala 2.jpg"},
+  //     {"Name": "Kerala", "Image": "images/kerala 3.jpeg"},
+  //     {"Name": "Kerala", "Image": "images/kerala 4.jpg"},
+  //   ],
+  //   [
+  //     {"Name": "Kutch", "Image": "images/kutch 1.jpeg"},
+  //     {"Name": "Kutch", "Image": "images/kutch 2.jpeg"},
+  //     {"Name": "Kutch", "Image": "images/kutch 3.jpeg"},
+  //     {"Name": "Kutch", "Image": "images/kutch 4.jpeg"},
+  //   ],
+  //   [
+  //     {"Name": "Qutub Minar", "Image": "images/qutub minar 1.jpg"},
+  //     {"Name": "Qutub Minar", "Image": "images/qutub minar 2.jpeg"},
+  //     {"Name": "Qutub Minar", "Image": "images/qutub minar 3.jpg"},
+  //     {"Name": "Qutub Minar", "Image": "images/qutub minar 4.jpg"},
+  //   ],
+  //   [
+  //     {"Name": "Somnath Mandir", "Image": "images/somnath 1.jpeg"},
+  //     {"Name": "Somnath Mandir", "Image": "images/somnath 2.jpeg"},
+  //     {"Name": "Somnath Mandir", "Image": "images/somnath 3.png"},
+  //     {"Name": "Somnath Mandir", "Image": "images/somnath 4.jpeg"},
+  //   ],
+  //   [
+  //     {"Name": "Gir", "Image": "images/gir 1.jpeg"},
+  //     {"Name": "Gir", "Image": "images/gir 2.jpeg"},
+  //     {"Name": "Gir", "Image": "images/gir 3.jpeg"},
+  //     {"Name": "Gir", "Image": "images/gir 4.jpeg"},
+  //   ],
+  // ];
+
+}
